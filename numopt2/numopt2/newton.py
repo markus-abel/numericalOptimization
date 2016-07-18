609152ec19adabf064d3f3ba2f66597fbb030f25
Markus <markus.abel@ambrosys.de>
2016-07-18 11:15:30 +0200

    added update

--- numopt2/numopt2/newton.py
+++ numopt2/numopt2/newton.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def steepest_descent(x0,f,grad_f,stop_criterium,max_iter):
+def newton(x0,f,grad_f,hesse,stop_criterium=0.001,max_iter=1000):
 
     x=x0
     # teste, ob delX > stopCriterium
@@ -10,10 +10,28 @@ def steepest_descent(x0,f,grad_f,stop_criterium,max_iter):
     alpha=0.1
     while delX > stop_criterium and iter< max_iter:
         xalt=x
-        direction= - grad_f(x)/abs(f(x))
+
+        direction= - grad_f(x)*inverse(hesse(x))
+        alpha = update_alpha(x,f,direction)
         #x^{n+1} = x^n + ..
         x = x + alpha*direction
         delX = np.linalg.norm(x-xalt)
         iter = iter + 1
 
     return x,iter
+
+
+def inverse(hesse):
+    if type(hesse) == np.array:
+        return np.inv(hesse)
+    else:
+        return 1. / hesse
+
+
+def update_alpha(x,f,direction):
+    alpha = 1
+    factor = 0.5
+    while f(x+alpha*direction) >= f(x):
+        alpha=factor*alpha
+    return alpha
+
--- numopt2/numopt2/steepest_descent.py
+++ numopt2/numopt2/steepest_descent.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def steepest_descent(x0,f,grad_f,stop_criterium,max_iter):
+def steepest_descent(x0,f,grad_f,stop_criterium=0.0001,max_iter=1000):
 
     x=x0
     # teste, ob delX > stopCriterium
@@ -11,8 +11,18 @@ def steepest_descent(x0,f,grad_f,stop_criterium,max_iter):
         xalt=x
         direction= - grad_f(x)/abs(f(x))
         #x^{n+1} = x^n + ..
-        x = x + alpha*direction
+
+        x = x + update_alpha(x,f,direction)*direction
         delX = np.linalg.norm(x-xalt)
         iter = iter + 1
 
     return x,iter
+
+
+
+def update_alpha(x,f,direction):
+    alpha = 1
+    factor = 0.5
+    while f(x+alpha*direction) >= f(x):
+        alpha=factor*alpha
+    return alpha
