1a67d15ffe6146da4bf23b12231e408524b94771
Tino <tino.goetzke@web.de>
2016-07-23 16:05:42 +0200

    Better setup

--- install.sh
+++ install.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-conda create python=3.5 --name numopt --yes --file requirements.txt
+set -e
+ENV=${1:-numopt}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function check_conda {
+  set +e
+  conda=$(which conda)
+  set -e
+  if [[ -z "$conda" ]]; then
+    echo "Didn't found conda! Please install miniconda first."
+    exit 1
+  fi
+}
+
+function make_env {
+  conda config --set always_yes true
+  if conda env list | awk {'print $1'} | grep -q $ENV; then
+      echo "Found old enviroment! It will be removed and a clean new one will be installed."
+      conda remove --name "$ENV" --all --quiet
+  fi
+  echo "Creating Enviroment: $ENV"
+  conda create python=3.6 --name $ENV --quiet
+  source activate $ENV
+  pip install -e $DIR
+  source deactivate
+
+}
+
+echo "Install numerical Optimization"
+check_conda
+make_env
--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='optimierung',
-      version='0.1',
-      description='lecture on optimization collection of methods',
+
+def read(fname, as_list=False):
+    """Read text file and return content as string."""
+    import os
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        content = f.read()
+    return content.split('\n') if as_list else content
+
+
+setup(name='numOpt',
+      version='0.2',
+      description='Collection of Methods and Example Code for the Lecture on Optimization',
       url='https://github.com/markus-abel/numericalOptimization',
-      author='Studeten der Universitaet Potsdam',
-      author_email='markus.abel@uni-potsdam.de',
-      license='TBD',
+      author='Students of the University Potsdam',
+      author_email='markus.abel@uni-potsdam.de, tino.goetzke@uni-potsdam.de',
+      license='MIT',
       zip_safe=False,
-      packages=find_packages(exclude=('tests',)),
+      packages=['numopt'],
+      python_requires='>=3.6',
+      install_requires=read('requirements.txt', as_list=True),
       )
